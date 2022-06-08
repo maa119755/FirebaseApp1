edce612337df0fb69832dc90f3f1d7b9d5084ebf
Mitsevich A <Vbwtdbx@outlook.com>
2022-06-08 14:15:12 +0300

    Firebase.database

--- app/src/main/java/com/example/firebaseapp1/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.firebaseapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    val database = Firebase.database
+    val myRef = database.getReference("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myRef.setValue("Hello, World!")
+        myRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d("TAG", snapshot.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
