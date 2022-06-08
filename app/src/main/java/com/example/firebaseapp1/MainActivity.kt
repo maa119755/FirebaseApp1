9b77f94db38bf7a2ed8b19e1b997d28fc23cc0ec
Mitsevich A <Vbwtdbx@outlook.com>
2022-06-08 16:00:23 +0300

    The 'kotlin-android-extensions' Gradle plugin is deprecated

--- app/src/main/java/com/example/firebaseapp1/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp1/MainActivity.kt
@@ -1,25 +1,29 @@
 package com.example.firebaseapp1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val database = Firebase.database
     val myRef = database.getReference("")
 
+    private var i = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        myRef.setValue("Hello, World!")
+        button.setOnClickListener { myRef.setValue(i++) }
         myRef.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 Log.d("TAG", snapshot.toString())
+                textViewSnapshot.text = snapshot.toString()
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -28,4 +32,5 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
+
 }
